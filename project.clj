00ec9059200b16baee40a958febe86c0a6da14d4
Jeff Mataya <jeff.mataya@gmail.com>
2020-10-17 16:12:36 -0400

    Handle environment variables

--- project.clj
+++ project.clj
@@ -4,8 +4,13 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [environ "1.2.0"]
                  [clj-http "3.10.3"]
                  [slingshot "0.12.2"]]
   :main ^:skip-aot sanilac.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :plugins [[lein-environ "1.2.0"]]
+  :profiles {:uberjar {:aot :all}
+             :dev [:project/dev :profiles/dev]
+             :profiles/dev {}
+             :project/dev {}})
--- src/sanilac/core.clj
+++ src/sanilac/core.clj
@@ -1,20 +1,27 @@
 (ns sanilac.core
-  (:require [clj-http.client :as client])
+  (:require [clj-http.client :as client]
+            [environ.core :refer [env]])
   (:use [slingshot.slingshot :only [throw+ try+]])
   (:gen-class))
 
-(defn get-request [url]
-  (try+
-    (client/get url)
-    (catch [:status 403] {:keys [request-time headers body]}
-      (println (str "403" request-time " " headers)))
-    (catch [:status 404] {:keys [request-time]}
-      (println (str "404" request-time " " url)))
-    (catch Object _
-      (println "Unexpected error"))))
+(defn query-psi
+  "Makes an API request to the Google Page Speed Insights API for a requested URL."
+  [url api-key]
+  (let [request-url (str "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" url "&key=" api-key)]
+    (client/get request-url)))
+
+;(defn get-request [url]
+;  (try+
+;    (client/get url)
+;    (catch [:status 403] {:keys [request-time headers body]}
+;      (println (str "403" request-time " " headers)))
+;    (catch [:status 404] {:keys [request-time]}
+;      (println (str "404" request-time " " url)))
+;    (catch Object _
+;      (println "Unexpected error"))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (get-request "https://www.example.com")
-  (println "Hello, World!"))
+  (let [psi-api-key (env :psi-api-key)]
+    (query-psi "https://www.bankrate.com" psi-api-key)))
