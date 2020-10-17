d473c70de06ba2520d47b89c6542214035c87944
Jeff Mataya <hi@jeffmataya.com>
2020-10-17 15:00:45 -0400

    Initial commit

---
+++ src/sanilac/core.clj
@@ -0,0 +1,7 @@
+(ns sanilac.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/sanilac/core_test.clj
@@ -0,0 +1,7 @@
+(ns sanilac.core-test
+  (:require [clojure.test :refer :all]
+            [sanilac.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
